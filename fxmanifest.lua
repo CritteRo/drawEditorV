6e0dae1a7119a5e2b70bcfce30d0f4d47321b872
CritteRo <critteras@gmail.com>
2021-08-19 15:50:28 +0300

    add: main structure / warmenu dependency

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'CritteR / CritteRo'
+description 'drawEditorV helps you easily create, modify and export to lua text and other drawables.'
+
+client_scripts {
+    'util/warmenu.lua',
+    'util/cl_functions.lua',
+    'client/cl_editor_menu.lua',
+    'client/cl_editor_main.lua',
+}
